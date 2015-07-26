604e8c9a5933a2ac410b106f349d82c51f4953df
AnnaMonika <AnnaMonika@users.noreply.github.com>
2015-07-26 08:55:04 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,8 @@
-#this function creates a matrix instance represented by and accessed by 
+#2 functions below tackle the problem of invertign large matrices and avoid re-doing it 
+#especially if it was already once calculated
+
+
+#this function 'makeCacheMatrix' creates a matrix instance represented by and accessed by 
 # a list of fields given in a last row of makeCacheMatrix 
 
 # set is a function which sets the matrix to be inverted
@@ -22,14 +26,13 @@ makeCacheMatrix <- function(x = matrix()){
        getInv = getInv)
 }
 
-#this function returns the inverse of the given matrix
+#the function 'cacheSolve' returns the inverse of the given matrix
 #but first checks if there is a need to invert it
 #by making sure whether the getInv field of a matrix instance is null or not
-# m equal to null means that one needs to invert tha matrix
-#otherwise it uses the matrix that was clculated earlier
+#getInv field equal to null means that one needs to invert the matrix
+#otherwise getInv keeps track of this inverted matrix that was calculated earlier
 #inverse is calculated by using 'solve' function 
-#after using 'solve' 'm 'is set to this inverse matrix and no longer nulll 
- 
+#after using 'solve' we use setInv to memorise the solution 
 
 cacheSolve <- function(x, ...) {
   m <- x$getInv()
